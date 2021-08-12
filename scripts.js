2e5f57a8f0593055c0aa74791bfed466c492885a
MastersuppVt <animastercompany@gmail.com>
2021-08-11 20:48:13 -0400

    base

---
+++ scripts.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+    document.getElementById("cargadoFull").classList.add("cargadoFull2");
+    console.log("hola")},5000);
+window.addEventListener("load", function(){
+         document.getElementById("loader").classList.toggle("loader2")
+    }) 
+ScrollReveal().reveal('.logo',{ delay:500 });
+ScrollReveal().reveal('.secUno',{ delay:500 });
+ScrollReveal().reveal('.secDos',{ delay:500 });
+ScrollReveal().reveal('.secTres',{ delay:500 });
+ScrollReveal().reveal('.secCuatro',{ delay:500 });
+ScrollReveal().reveal('.secCinco',{ delay:500 });
+ScrollReveal().reveal('.secSeis',{ delay:500 });
+ScrollReveal().reveal('.secSiete',{ delay:500 });
+ScrollReveal().reveal('.secOcho',{ delay:500 });
